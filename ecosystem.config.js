7b5d0ac15e3518ca5cb3bf612ee41742062306c2
vanilla-ice <valeraerohin97@mail.ru>
2018-10-30 13:45:30 +0300

    Feature: add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps : [
+    {
+      name: "nuxt",
+      script: "./node_modules/nuxt/bin/nuxt-start",
+      env: {
+          "HOST": "142.93.13.232",
+          "PORT": 3000,
+          "NODE_ENV": "production",
+          "BASE_URL": 'http://medach.pro'
+      }
+    }
+]
+}
+
