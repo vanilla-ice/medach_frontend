179e4ba69317feda0399bf800f6df278dba051de
vanilla-ice <valeraerohin97@mail.ru>
2018-10-30 15:46:47 +0300

    Fix: change port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "./node_modules/nuxt/bin/nuxt-start",
       env: {
           "HOST": "142.93.13.232",
-          "PORT": 3000,
+          "PORT": 1337,
           "NODE_ENV": "production",
           "BASE_URL": 'http://medach.pro'
       }
