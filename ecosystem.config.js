fc7e1301f51366c1df1f1996652805fda5dd95f7
vanilla-ice <valeraerohin97@mail.ru>
2018-10-30 16:19:11 +0300

    Fix: remove port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,10 @@ module.exports = {
       script: "./node_modules/nuxt/bin/nuxt-start",
       env: {
           "HOST": "142.93.13.232",
-          "PORT": 1337,
           "NODE_ENV": "production",
           "BASE_URL": 'http://medach.pro'
       }
     }
-]
+  ]
 }
 
