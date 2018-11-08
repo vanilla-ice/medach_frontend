7d84f50b74649bb197045ef8f67743e7b64e6a2b
vanilla-ice <valeraerohin97@mail.ru>
2018-11-08 15:49:19 +0300

    Feature: add 404

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,13 +1,56 @@
-<template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
-    <h1 v-else>Произошла ошибка</h1>
-    <nuxt-link to="/">Вернуться на главную</nuxt-link>
-  </div>
+<template lang="pug">
+.inner
+  the-header
+  .content
+    .title
+      | Упс...
+      .status-code
+        | {{error.statusCode}}
+      |Кажется Вы не там ищете, попробуйте еще раз с 
+      nuxt-link.link(to="/") главной
 </template>
 
 <script>
+import TheHeader from '~/components/TheHeader';
+
 export default {
-  props: ['error']
+  props: ['error'],
+  components: {
+    TheHeader
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.inner {
+  min-height: calc(100vh - 104px);
+  display: flex;
+  flex-flow: column nowrap;
+} 
+
+.content {
+  display: flex;
+  flex-flow: row nowrap;
+  flex: 1 1 auto;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  text-align: center;
+  font-size: 32px;
+}
+
+.link {
+  display: block;
+  text-align: center;
+  color: #7198BA;
+}
+
+.status-code {
+  font-size: 82px;
+  color: #7198BA;
+  font-weight: bold;
+  margin: 20px 0;
+}
+</style>
