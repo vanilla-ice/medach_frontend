dc82df35657252c8efff09b2a3472fe3bfb84f01
vanilla-ice <valeraerohin97@mail.ru>
2018-12-10 15:37:14 +0300

    Fix: ads script error

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    __dangerouslyDisableSanitizers: ["script"]
   },
   /*
   ** Customize the progress bar color
