96c64c209d949f462a3830e4643ac0fb2924fab9
Egor Romanov <unsaidxpl@gmail.com>
2019-01-22 10:25:39 +0300

    Temp: redirect imc profile

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -6,4 +6,9 @@ export default function ({ app, redirect }) {
       routerName === 'media-post-id') {
     return redirect(`/post/${app.router.currentRoute.params.id}`)
   }
+
+  // TODO remove and make real routing for this
+  if (app.router.currentRoute.path === '/profile/imc') {
+    return redirect('/profile/34');
+  }
 }
