7c07696337dceb757a3cb92b6ade618e169d46ce
pelevin-d-y <natpuot1992@gmail.com>
2019-02-15 11:55:51 +0300

    article contents hidden

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -32,7 +32,7 @@
         span(v-if="article.translate && article.translate !== ''")
          | Перевод: {{article.translate}}
 
-    .contents(v-if="true")
+    .contents(v-if="false")
       TheArticleContents
 
     .article-wrapper
