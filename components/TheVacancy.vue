31d864f20f8ef60c6e0a259bd72de9c95cc76ad0
pelevin-d-y <natpuot1992@gmail.com>
2020-04-17 20:17:56 +0300

    mobile vacancies

--- components/TheVacancy.vue
+++ components/TheVacancy.vue
@@ -116,4 +116,48 @@ export default {
     background: #5B5B5B;
   }
 }
+
+@media (max-width: 580px) {
+  .vacancy {
+    padding: 16px 14px 16px 14px;
+  }
+
+  .employer {
+    font-size: 16px;
+    line-height: 22px;
+  }
+
+  .title {
+    font-size: 24px;
+    line-height: 30px;
+  }
+
+  .vacancy__footer {
+    flex-flow: column wrap;
+    margin-top: 25px;
+
+    font-size: 16px;
+    line-height: 20px;
+  }
+
+  .vacancy__footer-top {
+    flex-flow: column wrap;
+    align-items: flex-start;
+    margin-bottom: 5px;
+  }
+
+  .location {
+    margin-bottom: 5px;
+  }
+
+  .location:after {
+    display: none;
+  }
+
+  .vacancy__footer-button {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 24px;
+  }
+}
 </style>
